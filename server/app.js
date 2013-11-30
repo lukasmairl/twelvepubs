020f612cad706aacb2e88548c30afc650683bf45
John Grogan <juannerito@gmail.com>
2013-11-30 18:36:34 -0500

    added maps, first pass

--- server/app.js
+++ server/app.js
@@ -4,12 +4,17 @@
  */
 
 var express = require('express');
+var winston = require('winston');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var gm = require('googlemaps');
+var util = require('util');
+var pubs =  require('./data/pubs');
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -22,7 +27,17 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-var config = {
+
+var googleMapsConfig = {
+  'key': 'AIzaSyCXKOd7viJVuC_fjYDWQfy57IZTLNzoEzE'
+}
+
+// Google maps
+gm.config(googleMapsConfig);
+
+
+// Four Square
+var foursquareConfig = {
   'secrets' : {
     'clientId' : 'J00O10IPKZIZHTIKUS3NYZ3B1ODHTCIUU1OO2J1UNPVQXFPX',
     'clientSecret' : 'TR5T2G4H3TJ3K4UE2XLEQWGHH2RWRH0W3IEQ5MA1D4VHYHTE',
@@ -30,36 +45,46 @@ var config = {
   }
 }
 
-var foursquare = require('node-foursquare')(config);
+var foursquare = require('node-foursquare');
+foursquare(foursquareConfig);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
 
-//foursquare
+// routing
+app.get('/', function(req, res){
+   res.render('index', { title: 'The 12 Pubs of Christmas', pubs: pubs, gmaps:googleMapsConfig.key});
+});
+
 app.get('/login', function(req, res) {
   res.writeHead(303, { 'location': foursquare.getAuthClientRedirectUrl() });
   res.end();
 });
 
+
 app.get('/callback', function (req, res) {
   foursquare.getAccessToken({
     code: req.query.code
-  }, 
+  },
   function (error, accessToken) {
    	if(error) {
       res.send('An error was thrown: ' + error.message);
     }
     else {
-      res.render('index', {accessToken: accessToken});
+      foursquare.Checkins.getRecentCheckins(null, accessToken, function(data){
+         console.log(data);
+         res.render('index', {accessToken: accessToken, data:data});
+      });
     }
   });
 });
 
 
+
+// Create server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,7 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The 12 Pubs of Christmas', pubs: pubs});
 };
