070d3c49a50294c63127cceb82e385167c9f1677
HUGE | John Grogan <jgrogan@hugeinc.com>
2013-12-03 17:13:51 -0500

    added redis for production - part 2

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var admin = require('./controllers/admin');
 if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 	var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+
+	redis.auth(rtg.auth.split(":")[1]);
 } else {
 	var redisClient = require('redis').createClient();
 }
