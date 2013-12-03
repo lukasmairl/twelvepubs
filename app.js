143cac668b6a919a81da6095eee3f79ca5561737
HUGE | John Grogan <jgrogan@hugeinc.com>
2013-12-03 17:15:39 -0500

    added redis for production - once more with feeling

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 	var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
 
-	redis.auth(rtg.auth.split(":")[1]);
+	redisClient.auth(rtg.auth.split(":")[1]);
 } else {
 	var redisClient = require('redis').createClient();
 }
