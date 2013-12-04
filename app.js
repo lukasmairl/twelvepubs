9bb9688526fc68f03d00244e0cee1854399c338f
HUGE | John Grogan <jgrogan@hugeinc.com>
2013-12-03 19:28:48 -0500

    scehdule/foursquare

--- app.js
+++ app.js
@@ -10,34 +10,23 @@ var http = require('http');
 var path = require('path');
 // var instagram = require('instagram-node-lib');
 // var foursquare = require('node-foursquare');
-var gm = require('googlemaps');
 var util = require('util');
 var pubs = require('./data/pubs');
 var admin = require('./controllers/admin');
 
-console.log(process.env.REDISTOGO_URL);
-
-// var redisClient = require('redis-url').connect(process.env.REDISTOGO_URL);
-
-// redisClient.set('foo', 'bar');
-
-// redisClient.get('foo', function(err, value) {
-//   console.log('foo is: ' + value);
-// });
-
-
-
 if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 	var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-	console.log(rtg.port, rtg.hostname, rtg.auth);
 	redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+	var redisClient = require("redis").createClient();
 }
+
+//stash the pubs in Redis
 redisClient.set('pubs', JSON.stringify(pubs));
 
-
+// instantiate the express app.
 var app = express();
-
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -54,9 +43,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 var googleMapsConfig = {
   'key': 'AIzaSyCXKOd7viJVuC_fjYDWQfy57IZTLNzoEzE'
 };
-
-// Google maps
-gm.config(googleMapsConfig);
 
 // development only
 if ('development' == app.get('env')) {
@@ -75,6 +61,11 @@ require('./routes/pubs')(app,redisClient);
 require('./routes/admin')(app, admin, redisClient, pubs);
 
 require('./routes/map')(app, {pubs: pubs, gmaps: googleMapsConfig.key});
+
+require('./routes/schedule')(app, {pubs: pubs});
+
+require('./routes/foursquare')(app);
+
 
 //require('./routes/api')(app);
 
--- models/api.js
+++ models/api.js
@@ -1,105 +1,105 @@
-//
-//  Api
-//
-var _ = require('underscore');
-var twitter = require('twit');
-var instagram = require('instagram-node-lib');
-var foursquare = require('node-foursquare');
+// //
+// //  Api
+// //
+// var _ = require('underscore');
+// var twitter = require('twit');
+// var instagram = require('instagram-node-lib');
+// var foursquare = require('node-foursquare');
 
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var client = require("redis").createClient(rtg.port, rtg.hostname);
-  console.log(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
-} else {
-  var client = require('redis').createClient();
-}
+// if (process.env.REDISTOGO_URL) {
+//   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+//   var client = require("redis").createClient(rtg.port, rtg.hostname);
+//   console.log(rtg.port, rtg.hostname);
+//   client.auth(rtg.auth.split(":")[1]);
+// } else {
+//   var client = require('redis').createClient();
+// }
 
 
 
-// Twitter
-var T = new twitter({
-    consumer_key: '4WLbGgPyRZ1RbCQ8J2i1kA'
-  , consumer_secret: 'bwXmWVXWhujAy4UXXEY1EiAf0BFfOBo3t0lF0sVo'
-  , access_token: '133161437-j28mPeyE886AtwQmA4BbJc7wsx348wlgdjOT90XM'
-  , access_token_secret: 'XU3lDMEDGkGVbptSTbpUxrXEtGuJNX0WPIfZ2JI7AhQ2w'
-});
+// // Twitter
+// var T = new twitter({
+//     consumer_key: '4WLbGgPyRZ1RbCQ8J2i1kA'
+//   , consumer_secret: 'bwXmWVXWhujAy4UXXEY1EiAf0BFfOBo3t0lF0sVo'
+//   , access_token: '133161437-j28mPeyE886AtwQmA4BbJc7wsx348wlgdjOT90XM'
+//   , access_token_secret: 'XU3lDMEDGkGVbptSTbpUxrXEtGuJNX0WPIfZ2JI7AhQ2w'
+// });
 
-// Instagram
-instagram.set('client_id', '4e66c925fae5428dbe11a4f51c9ea7ce');
-instagram.set('client_secret', '3e0c84a3cb514a2ab9ca76f62cf1b1cd');
+// // Instagram
+// instagram.set('client_id', '4e66c925fae5428dbe11a4f51c9ea7ce');
+// instagram.set('client_secret', '3e0c84a3cb514a2ab9ca76f62cf1b1cd');
 
-var Api = function() {}
+// var Api = function() {}
 
-Api.prototype.getTweets = function() {
-  console.log("------------------- get tweets -------------------");
-  var self = this;
+// Api.prototype.getTweets = function() {
+//   console.log("------------------- get tweets -------------------");
+//   var self = this;
 
-  T.get('search/tweets', { q: '#TwelvePubs', count: 100 }, function(err, res) {
-      console.log("---- data ----");
-      console.log(res);
+//   T.get('search/tweets', { q: '#TwelvePubs', count: 100 }, function(err, res) {
+//       console.log("---- data ----");
+//       console.log(res);
 
-      var tweets = res.statuses;
+//       var tweets = res.statuses;
 
-      _.each(tweets, function(tweet) {
+//       _.each(tweets, function(tweet) {
 
-        var text    = tweet.text;
-        var createdAt   = tweet.created_at;
-        var user    = tweet.user;
-        var date = (new Date(tweet.created_at)).getTime() / 1000;
+//         var text    = tweet.text;
+//         var createdAt   = tweet.created_at;
+//         var user    = tweet.user;
+//         var date = (new Date(tweet.created_at)).getTime() / 1000;
 
-        var tweet = {
-          type: "TWEET",
-          text : text,
-          createdAt : date,
-          user: user.screen_name
-        };
+//         var tweet = {
+//           type: "TWEET",
+//           text : text,
+//           createdAt : date,
+//           user: user.screen_name
+//         };
 
-        self.addKey(tweet);
+//         self.addKey(tweet);
 
-      });
-  });
+//       });
+//   });
 
-}
+// }
 
-Api.prototype.getInstagrams = function() {
-    var self = this;
+// Api.prototype.getInstagrams = function() {
+//     var self = this;
 
-    instagram.tags.recent({
-        q: 'zambrano',
-        complete: function(instagrams){
-          _.each(instagrams, function(instagram) {
+//     instagram.tags.recent({
+//         q: 'zambrano',
+//         complete: function(instagrams){
+//           _.each(instagrams, function(instagram) {
 
-            var user    = instagram.user;
-            var createdAt   = instagram.created_time;
-            var image     = instagram.images.standard_resolution;
+//             var user    = instagram.user;
+//             var createdAt   = instagram.created_time;
+//             var image     = instagram.images.standard_resolution;
 
-            var instagram = {
-              type: "INSTAGRAM",
-              image: image,
-              createdAt: createdAt,
-              user: user.username
-            };
+//             var instagram = {
+//               type: "INSTAGRAM",
+//               image: image,
+//               createdAt: createdAt,
+//               user: user.username
+//             };
 
-            self.addKey(instagram);
-      });
-    }
-  });
-};
+//             self.addKey(instagram);
+//       });
+//     }
+//   });
+// };
 
-Api.prototype.addKey = function(key) {
+// Api.prototype.addKey = function(key) {
 
-    var args = [ 'userfeed2', key.createdAt, JSON.stringify(key) ];
-    client.zadd(args, function (err, response) {
-        if (err) throw err;
-        console.log('added '+response+' items.');
-    });
+//     var args = [ 'userfeed2', key.createdAt, JSON.stringify(key) ];
+//     client.zadd(args, function (err, response) {
+//         if (err) throw err;
+//         console.log('added '+response+' items.');
+//     });
 
-};
+// };
 
-Api.prototype.queryApis = function() {
-  var tweets = this.getTweets();
-  var instagrams = this.getInstagrams();
-}
+// Api.prototype.queryApis = function() {
+//   var tweets = this.getTweets();
+//   var instagrams = this.getInstagrams();
+// }
 
-module.exports = Api;
+// module.exports = Api;
