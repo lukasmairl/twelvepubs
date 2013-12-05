566279b648740090c131b705680e31ac8b95f7ee
HUGE | John Grogan <jgrogan@hugeinc.com>
2013-12-04 20:11:54 -0500

    bloop

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,7 +4,7 @@
 module.exports = function(app, config) {
 
   app.get('/schedule', function(req, res) {
-    res.render('schedule', { title: 'The 12 Pubs of Christmas', pubs: config.pubs});
+    res.render('schedule', { title: 'The 12 Pubs of Christmas', pubs: config.pubs, slug:'schedule'});
   });
 
 };
